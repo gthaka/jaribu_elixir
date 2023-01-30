76bc1d208c6d830ee74cc7644a920bed1af30753
Anthony G <agithaka@live.com>
2023-01-30 21:57:41 +0300

    FizzBuzz module

---
+++ FizzBuzz.ex
@@ -0,0 +1,16 @@
+defmodule FizzBuzz do
+  def run(n) do
+    1..n |> Enum.map(&fizzbuzz/1) |> Enum.join(", ")
+  end
+
+  defp fizzbuzz(n) do
+    case {rem(n, 3) == 0, rem(n, 5) == 0} do
+      {true, true} -> "FizzBuzz"
+      {true, false} -> "Fizz"
+      {false, true} -> "Buzz"
+      _ -> Integer.to_string(n)
+    end
+  end
+end
+
+IO.puts(FizzBuzz.run(100))
